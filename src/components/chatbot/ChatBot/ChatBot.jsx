1629d0f8453b2b83b0239777479166a0a67aee5a
AmelMahmutovic555 <152235848+AmelMahmutovic555@users.noreply.github.com>
2026-01-05 01:56:47 +0100

    Update ChatBot.jsx

--- src/components/chatbot/ChatBot/ChatBot.jsx
+++ src/components/chatbot/ChatBot/ChatBot.jsx
@@ -145,6 +145,7 @@ export default function ChatBot() {
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                   placeholder="Write your message..."
+                  required
                 />
 
                 {loading ? (
